d8e81bd9de4c9cb49635616966e4d21d3f743417
Carlos Cobo <toqueteos@gmail.com>
2016-02-23 00:34:33 +0100

    chmod +x on etcd scripts + missing setup steps

--- etcd-setup.sh
+++ etcd-setup.sh
@@ -10,3 +10,5 @@ cd $HOME/etcd;
 ./etcdctl --timeout "5s" set foo/foo "Hello"
 ./etcdctl --timeout "5s" set foo/bar 2
 ./etcdctl --timeout "5s" set foo/qux 4.5
+./etcdctl --timeout "5s" mkdir bar/qux
+./etcdctl --timeout "5s" set bar/qux/hello "World"
