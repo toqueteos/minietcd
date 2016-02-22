844f436c04033fe146d95d0697de31a48d03f19b
Carlos Cobo <toqueteos@gmail.com>
2016-02-23 00:52:23 +0100

    etcd-install: Move required binaries to proper path

--- etcd-install.sh
+++ etcd-install.sh
@@ -5,10 +5,11 @@ set -e
 ETCD_CACHE_DIR=${HOME}/etcd
 
 if [ ! -d ${ETCD_CACHE_DIR} ]; then
-    mkdir ${ETCD_CACHE_DIR};
-    cd ${ETCD_CACHE_DIR};
-    curl -L https://github.com/coreos/etcd/releases/download/v2.2.5/etcd-v2.2.5-linux-amd64.tar.gz -o ${ETCD_CACHE_DIR}/etcd-v2.2.5-linux-amd64.tar.gz
-    tar xzvf etcd-v2.2.5-linux-amd64.tar.gz -C ${ETCD_CACHE_DIR};
+    mkdir ${ETCD_CACHE_DIR}
+    curl -L https://github.com/coreos/etcd/releases/download/v2.2.5/etcd-v2.2.5-linux-amd64.tar.gz -o etcd-v2.2.5-linux-amd64.tar.gz
+    tar xzvf etcd-v2.2.5-linux-amd64.tar.gz
+    mv etcd-v2.2.5-linux-amd64/etcdctl ${ETCD_CACHE_DIR}/etcdctl
+    mv etcd-v2.2.5-linux-amd64/etcd ${ETCD_CACHE_DIR}/etcd
 else
     echo 'Using cached etcd';
 fi
