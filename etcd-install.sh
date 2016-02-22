c54adf483b1dc78113ca57fc5cc3711861a0ee1a
Carlos Cobo <toqueteos@gmail.com>
2016-02-23 00:48:16 +0100

    etcd-install: cd was required

--- etcd-install.sh
+++ etcd-install.sh
@@ -6,6 +6,7 @@ ETCD_CACHE_DIR=${HOME}/etcd
 
 if [ ! -d ${ETCD_CACHE_DIR} ]; then
     mkdir ${ETCD_CACHE_DIR};
+    cd ${ETCD_CACHE_DIR};
     curl -L https://github.com/coreos/etcd/releases/download/v2.2.5/etcd-v2.2.5-linux-amd64.tar.gz -o ${ETCD_CACHE_DIR}/etcd-v2.2.5-linux-amd64.tar.gz
     tar xzvf etcd-v2.2.5-linux-amd64.tar.gz -C ${ETCD_CACHE_DIR};
 else
